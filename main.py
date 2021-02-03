e6a3c9df45b464cad262c2369ffb6da55ebaea37
A. Alfonso F. R <alffore@gmail.com>
2021-02-03 14:47:01 -0600

    ajustes para iterador

--- main.py
+++ main.py
@@ -317,7 +317,7 @@ hidden_size = 256
 encoder1 = enc.EncoderRNN(input_lang.n_words, hidden_size).to(device)
 attn_decoder1 = dec.AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
 
-trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
+trainIters(encoder1, attn_decoder1, 100000, print_every=5000)
 
 evaluateRandomly(encoder1, attn_decoder1)
 
